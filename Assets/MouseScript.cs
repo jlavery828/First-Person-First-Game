c356f47b5b109c290032898a33fe5f5b30572bc2
jason lavery <jlavery828@gmail.com>
2026-09-22 12:08:28 -0400

    Initial Unity project

---
+++ Assets/MouseScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100;
+
+    public Transform playerBody;
+
+    float xRotation = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
